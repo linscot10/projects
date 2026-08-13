53807691a1481f2b0427dc6d72e100c17a0bd13d
linscot10 <linscot10@gmail.com>
2026-08-13 21:33:01 +0300

    Noteflow: Date updates

--- Notes App Pro/src/Pages/Home.jsx
+++ Notes App Pro/src/Pages/Home.jsx
@@ -1,10 +1,54 @@
-import React from 'react'
-import {Link} from "react-router-dom"
+import React, { useEffect, useState } from 'react'
+
+// import {Link} from "react-router-dom"
 
 const Home = () => {
+ const getTimeOfDay=(hour)=>{
+  if(hour>=5 && hour <12) return 'MORNING';
+  if(hour>=12 && hour <17) return 'AFTERNOON';
+  if(hour>=17 && hour <21) return 'EVENING';
+  return "NIGHT"
+ }
+
+ const getCurrentTime=()=>{
+  return getTimeOfDay(new Date().getHours())
+
+ }
+
+ const [timeOfDay, setTimeOfDay]=useState( getCurrentTime);
+
+ useEffect(()=>{
+  const interval=setInterval(()=>{
+    setTimeOfDay(getCurrentTime)
+  }, 60000);
+  return ()=> clearInterval(interval)
+ },[])
+
+//   const getTimeOfDay=(hour)=>{
+//     if(hour>=5 && hour<12) return "MORNING";
+//     if(hour>=12 && hour<17) return "AFTERNOON";
+//     if(hour>=17 && hour<21) return "EVENING";
+//     // if(hour>=5 && hour<12) return "MORNING";
+//     return "NIGHT"
+
+//   }
+
+//   const [timeOfDay, setTimeOfDay]=useState(()=>{
+//     return getTimeOfDay(new Date().getHours())
+//   })
+
+//   useEffect(()=>{
+// const interval=setInterval(()=>{
+//   setTimeOfDay(getTimeOfDay(new Date().getHours()))
+// }, 60000);
+// return () =>clearInterval(interval)
+
+//   },[])
   return (
     <div className='w-full bg-amber-200'>
-    Home
+    <h1 className='flex justify-center text-xl m-4'>GOOD {timeOfDay},  SCOTT!</h1>
+    <h2 className='flex justify-center text-lg m-4'>Welcome Back! Here's Your Workspace.</h2>
+
     </div>
   )
 }
--- Notes App Pro/src/components/Navbar.jsx
+++ Notes App Pro/src/components/Navbar.jsx
@@ -1,32 +1,34 @@
-import React from 'react'
-import {Link} from "react-router-dom"
+import React, { useState } from 'react'
+import {Link, useLocation} from "react-router-dom"
+
 
 const Navbar = () => {
+  const location =useLocation()
   return (
     <div>
         <nav className=" flex flex-col gap-4 bg-gray-400 w-48 h-screen">
             <div className='p-4'><h1 className='font-bold text-gray-600 text-xl'><span className='text-green-600'>NOTEFLOW</span>-<span>PRO</span></h1></div>
             <div className='flex flex-col gap-3 justify-center text-lg p-4'>
           <ul className="flex flex-col gap-2  w-40 " >
-          <Link  to={'/New_notes'}> <li className='hover:bg-white rounded-lg p-2  w-48 '>+ New Note </li>
+          <Link  to={'/New_notes'} className={location.pathname==='/New_notes'? "bg-white rounded-lg w-48":"bg-gray-400"}> <li className='hover:bg-white rounded-lg p-2  w-48 '>+ New Note </li>
 </Link>
-               <Link  to={'/Search'}> <li className='hover:bg-white rounded-lg p-2 w-48'>Search</li></Link> 
+               <Link  to={'/Search'} className={location.pathname==='/Search'? "bg-white rounded-lg w-48":"bg-gray-400"}> <li className='hover:bg-white rounded-lg p-2 w-48'>Search</li></Link> 
                 
                 
                 
             </ul>
             <ul className="flex flex-col gap-2  w-40 mt-5 " >
-            <Link  to={'/'}><li className='hover:bg-white rounded-lg p-2  w-48 '>Home</li> </Link> 
-                 <Link  to={'/My_Notes'}> <li className='hover:bg-white rounded-lg p-2 w-48'>  My Notes</li></Link>
-               <Link  to={'/Todos'}><li className='hover:bg-white rounded-lg p-2 w-48 '>Todos </li></Link>
-               <Link  to={'/Favorites'}> <li className='hover:bg-white rounded-lg p-2 w-48 '>Favorites </li></Link> 
+            <Link  to={'/'} className={location.pathname==='/'? "bg-white rounded-lg  w-48" : "bg-gray-400"}><li className='hover:bg-white rounded-lg p-2  w-48 '>Home</li> </Link> 
+                 <Link  to={'/My_Notes'} className={location.pathname==='/My_Notes'? "bg-white rounded-lg w-48":"bg-gray-400"}> <li className='hover:bg-white rounded-lg p-2 w-48'>  My Notes</li></Link>
+               <Link  to={'/Todos'} className={location.pathname==='/Todos'? "bg-white rounded-lg w-48":"bg-gray-400"}><li className='hover:bg-white rounded-lg p-2 w-48 '>Todos </li></Link>
+               <Link  to={'/Favorites'} className={location.pathname==='/Favorites'? "bg-white rounded-lg w-48":"bg-gray-400"}> <li className='hover:bg-white rounded-lg p-2 w-48 '>Favorites </li></Link> 
                 
                 
             </ul>
             <ul className="flex flex-col gap-2  w-40 mt-10" >
-                 <Link  to={'/Dark_Mode'}>  <li className='hover:bg-white rounded-lg p-2 w-48 '>Dark Mode  </li></Link>
-              <Link  to={'/Setting'}> <li className='hover:bg-white rounded-lg p-2 w-48 '>Setting </li> </Link>
-               <Link  to={'/Profile'}><li className='hover:bg-white rounded-lg p-2 w-48 '> Profile </li> </Link>
+                 <Link  to={'/Dark_Mode'} className={location.pathname==='/Dark_Mode'? "bg-white rounded-lg w-48":"bg-gray-400"}>  <li className='hover:bg-white rounded-lg p-2 w-48 '>Dark Mode  </li></Link>
+              <Link  to={'/Setting'} className={location.pathname==='/Setting'? "bg-white rounded-lg w-48":"bg-gray-400"}> <li className='hover:bg-white rounded-lg p-2 w-48 '>Setting </li> </Link>
+               <Link  to={'/Profile'} className={location.pathname==='/Profile'? "bg-white rounded-lg w-48":"bg-gray-400"}><li className='hover:bg-white rounded-lg p-2 w-48 '> Profile </li> </Link>
                 
             </ul>
 
